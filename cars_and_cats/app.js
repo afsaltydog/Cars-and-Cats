d1e04bb381867da370ccd29a75ad236406a67734
Andrew Ferguson <afsaltydog@gmail.com>
2019-04-08 16:09:17 -0500

    Add files via upload

---
+++ cars_and_cats/app.js
@@ -0,0 +1,111 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('./views/index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === "/cars") {
+        fs.readFile('./views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/cats") {
+        fs.readFile('./views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/cars/new") {
+        fs.readFile('./views/newCar.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if(request.url === '/images/cat-300572__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cat-300572__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cat-2083492__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cat-2083492__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/kitty-551554__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/kitty-551554__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/kitty-2948404__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/kitty-2948404__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/amg-1880381__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/amg-1880381__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/aston-2118857__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/aston-2118857__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/bmw-918408__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/bmw-918408__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/car-49278__340.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/car-49278__340.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 7077");
